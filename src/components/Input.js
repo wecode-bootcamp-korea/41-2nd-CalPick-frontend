1d72e3dc6135b59515bdd50a7c83fbc13d096b56
Mia <tjwlfif@gmail.com>
2023-01-30 22:01:44 +0900

    Add: input & label 공용 컴포넌트 구현 완료

---
+++ src/components/Input.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = ({ id, type, disabled = false, required = false }) => {
+  return (
+    <InputStyle id={id} type={type} disabled={disabled} required={required} />
+  );
+};
+
+const InputStyle = styled.input`
+  width: 340px;
+  height: 50px;
+  padding-left: 20px;
+  font-size: ${props => props.theme.fontSize.l};
+  color: ${props => props.theme.colors.primary};
+  font-weight: ${props => props.theme.fontWeight.bold};
+  border: 2px solid ${props => props.theme.colors.primary};
+  border-radius: 5px;
+
+  &:disabled {
+    outline: none;
+    background-color: ${props => props.theme.colors.control};
+    color: ${props => props.theme.colors.primary};
+  }
+
+  &:focus {
+    outline: none;
+    border: 3px solid ${props => props.theme.colors.primary};
+  }
+
+  &:invalid {
+    outline: none;
+    border: 3px solid ${props => props.theme.colors.warning};
+  }
+`;
+
+export default Input;
---
+++ src/components/Label.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = ({ htmlFor, children }) => {
+  return <LabelStyle htmlFor={htmlFor}>{children}</LabelStyle>;
+};
+
+const LabelStyle = styled.label`
+  font-size: ${props => props.theme.fontSize.l};
+  color: ${props => props.theme.colors.primary};
+  font-weight: ${props => props.theme.fontWeight.bold};
+`;
+
+export default Label;

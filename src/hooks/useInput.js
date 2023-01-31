d167b55ba81f830ed8a66af77199f98fff7e440a
Mia <tjwlfif@gmail.com>
2023-01-31 09:50:51 +0900

    Add: 공용 hook - useInput 생성

---
+++ src/hooks/useInput.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export const useInput = initialValue => {
+  const [info, setInfo] = useState(initialValue);
+  const handleInput = ({ target }) => {
+    const { name, value } = target;
+    setInfo(prev => ({ ...prev, [name]: value }));
+  };
+  const resetInputValue = () => setInfo(initialValue);
+  return { info, handleInput, resetInputValue };
+};

ff082a2d5d56fc7e7e8550e093da82e2121e1351
ijinkyung <ijinkyung@gmail.com>
2023-02-04 15:18:05 +0900

    Update : 시험명 클릭시 해당 시험일자 데이터 조건부 렌더링 기능 구현중

---
+++ src/pages/ExamList/Components/ExamPlan/ExamPlan.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+
+import ExamPlanList from '../ExamPlanList/ExamPlanList';
+import * as S from './ExamPlan.styles';
+
+export default function ExamPlan() {
+  const [dateList, setDateList] = useState([]);
+  const [examName, setExamName] = useState('캘픽');
+
+  useEffect(() => {
+    fetch(`http://10.58.52.136:3000/tests?testName=${examName}`)
+      .then(response => response.json())
+      .then(data => setDateList(data));
+  }, [examName]);
+
+  const changeHandler = e => {
+    setExamName(e.target.value);
+  };
+
+  return (
+    <>
+      <S.ExamTitleContainer>
+        {EXAM_LIST.map(title => {
+          return (
+            <S.ExamTitle>
+              <S.ExamInput
+                type="radio"
+                name="examtitle"
+                id={title.name}
+                value={title.title}
+                onChange={changeHandler}
+              ></S.ExamInput>
+              <S.ExamLabel htmlFor={title.name}>{title.title}</S.ExamLabel>
+            </S.ExamTitle>
+          );
+        })}
+      </S.ExamTitleContainer>
+      <S.TableTitle>시험 접수</S.TableTitle>
+      <S.TableContainer>
+        <S.ListItemContainer>
+          {TITLE_LIST.map(list => {
+            return <S.ListItemTitle>{list.title}</S.ListItemTitle>;
+          })}
+        </S.ListItemContainer>
+        {dateList.map(info => {
+          return (
+            <>
+              <S.ListItemContainer>
+                <ExamPlanList info={info} examName={examName}></ExamPlanList>
+              </S.ListItemContainer>
+            </>
+          );
+        })}
+      </S.TableContainer>
+    </>
+  );
+}
+
+const TITLE_LIST = [
+  {
+    id: 1,
+    title: '시험일',
+  },
+  {
+    id: 2,
+    title: '접수기간',
+  },
+  {
+    id: 3,
+    title: '성적발표일',
+  },
+  {
+    id: 4,
+    title: '시험접수',
+  },
+];
+
+const EXAM_LIST = [
+  {
+    id: 1,
+    title: '캘픽',
+    name: '캘픽',
+  },
+  {
+    id: 2,
+    title: '캘픽스피킹',
+    name: '캘픽스피킹',
+  },
+  {
+    id: 3,
+    title: '오캘픽',
+    name: '오캘픽',
+  },
+  {
+    id: 4,
+    title: '캘픽스',
+    name: '캘픽스',
+  },
+];
---
+++ src/pages/ExamList/ExamList.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ExamListContainer = styled.div`
+  position: relative;
+`;
+
+export const Logo = styled.img`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: -250px;
+  width: 150px;
+  height: 60px;
+`;

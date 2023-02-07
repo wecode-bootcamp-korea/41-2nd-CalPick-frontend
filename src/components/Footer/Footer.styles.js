b9ccf591ce949375f1821ac8cdbbebda80c57814
kws <raincastle1211@gmail.com>
2023-02-07 19:04:57 +0900

    [41기 김우성] Add: Footer 레이아웃 구현 완료
    상수데이터를 통해 수정완료

---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,193 @@
+import styled from 'styled-components';
+
+const S = {
+  Top: styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    margin-left: 76px;
+    margin-top: 67px;
+    display: flex;
+    -ms-flex-direction: column;
+  `,
+  TopFirst: styled.div`
+    display: flex;
+    position: relative;
+    flex-direction: row;
+  `,
+  UlFirst: styled.ul`
+    display: flex;
+    flex-direction: column;
+    width: 185px;
+  `,
+  LiFirst: styled.li`
+    display: inline-flex;
+    align-items: center;
+    margin: 10px 0;
+    font-weight: bold;
+    font-size: 13px;
+    line-height: 16px;
+    letter-spacing: -0.01em;
+    color: #787878;
+  `,
+  Center: styled.img`
+    width: 30px;
+    height: 30px;
+  `,
+  LiSecond: styled.li`
+    display: inline-flex;
+    -webkit-box-align: center;
+    align-items: center;
+    margin: 10px 0;
+    font-weight: bold;
+    font-size: 13px;
+    line-height: 16px;
+    letter-spacing: -0.01em;
+    color: #787878;
+  `,
+  Megaphone: styled.img`
+    width: 30px;
+    height: 30px;
+  `,
+  Second: styled.div`
+    display: flex;
+  `,
+  Service: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+  UlSecond: styled.ul`
+    display: flex;
+    flex-direction: column;
+  `,
+  LiThird: styled.li`
+    color: #787878;
+    font-size: 13px;
+    line-height: 16px;
+    letter-spacing: -0.01em;
+    padding: 6px 0;
+    margin-top: 10px;
+  `,
+  MapFirst: styled.p`
+    margin-bottom: 10px;
+  `,
+  Fi: styled.li`
+    display: flex;
+  `,
+  FiFirst: styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 100px;
+    margin-right: 100px;
+  `,
+  UlThird: styled.ul`
+    display: flex;
+    flex-direction: column;
+  `,
+  LiEight: styled.li`
+    color: #787878;
+    font-size: 13px;
+    line-height: 16px;
+    letter-spacing: -0.01em;
+    padding: 6px 0;
+    margin-top: 10px;
+  `,
+  MapSecond: styled.p`
+    margin-bottom: 10px;
+  `,
+  FiSecond: styled.li`
+    display: flex;
+    flex-direction: column;
+  `,
+  UlFourth: styled.ul`
+    display: flex;
+    flex-direction: column;
+  `,
+  LiTenth: styled.li`
+    color: #787878;
+    font-size: 13px;
+    line-height: 16px;
+    letter-spacing: -0.01em;
+    padding: 6px 0;
+    margin-top: 10px;
+  `,
+  MapThird: styled.p`
+    margin-bottom: 10px;
+  `,
+  App: styled.div`
+    position: absolute;
+    right: 100px;
+  `,
+  Facebook: styled.img`
+    width: 30px;
+    height: 30px;
+  `,
+  Instagram: styled.img`
+    width: 30px;
+    height: 30px;
+    margin-left: 10px;
+    margin-right: 10px;
+  `,
+  Youtube: styled.img`
+    width: 30px;
+    height: 30px;
+  `,
+  Bottom: styled.div`
+    margin-top: 60px;
+    margin-bottom: 20px;
+  `,
+  SpanFirst: styled.span`
+    font-size: 12px;
+  `,
+  Arrow: styled.img`
+    width: 10px;
+    height: 10px;
+    margin-left: 10px;
+  `,
+  UlFifth: styled.ul`
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+  `,
+  LiTwelfth: styled.li`
+    color: #787878;
+    font-size: 11px;
+    display: inline-flex;
+    height: 13px;
+    line-height: 13px;
+    letter-spacing: -0.01em;
+  `,
+  LiThirteenth: styled.li`
+    color: #787878;
+    font-size: 11px;
+    display: inline-flex;
+    height: 13px;
+    line-height: 13px;
+    letter-spacing: -0.01em;
+    margin-left: 10px;
+    margin-right: 10px;
+    font-weight: bold;
+  `,
+  LiFourteenth: styled.li`
+    color: #787878;
+    font-size: 11px;
+    display: inline-flex;
+    height: 13px;
+    line-height: 13px;
+    letter-spacing: -0.01em;
+    margin-right: 10px;
+  `,
+  LiFifteenth: styled.li`
+    color: #787878;
+    font-size: 11px;
+    display: inline-flex;
+    height: 13px;
+    line-height: 13px;
+    letter-spacing: -0.01em;
+  `,
+  SpanSecond: styled.span`
+    font-size: 10px;
+  `,
+};
+
+export default S;

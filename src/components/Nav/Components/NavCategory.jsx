1c4ed1dca840bdd90ceaf8e9201390aea270a4ad
Ha-Pyeongan <96smilepeace@gmail.com>
2023-02-08 15:52:48 +0900

    Update : Nav Router 경로 코드 추가

--- src/components/Nav/Components/NavCategory.jsx
+++ src/components/Nav/Components/NavCategory.jsx
@@ -1,38 +1,56 @@
 import React from 'react';
-import * as S from './NavCategory.styles';
+import { useNavigate } from 'react-router-dom';
 import NavCategoriesIcon from './NavCategoriesIcon';
 import { IoMdInformationCircleOutline } from 'react-icons/io';
 import { SlCalender } from 'react-icons/sl';
-import { ImMap2 } from 'react-icons/im';
+import { RiListCheck } from 'react-icons/ri';
 import { CgList } from 'react-icons/cg';
+import * as S from './NavCategory.styles';
 
 export default function NavCategory() {
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem('accessToken');
+
+  const goToMain = e => {
+    navigate('/');
+  };
+
+  const CATEGORY_ICON = [
+    {
+      icon: <IoMdInformationCircleOutline size="24px" />,
+      text: '고사장 안내',
+      router: '/examinfo',
+    },
+    { icon: <SlCalender size="24px" />, text: '캘린더', router: '/calendar' },
+    {
+      icon: <RiListCheck size="24px" />,
+      text: '전체 시험 일정',
+      router: '/examlist',
+    },
+    {
+      icon: <CgList size="24px" />,
+      text: '마이 페이지',
+      router: '/mypage',
+    },
+  ];
   return (
     <S.Category className="inner">
-      <div>
+      <div onClick={goToMain}>
         <S.LogoImg src="/images/logo.png" alt="캘픽 메인 로고" />
       </div>
 
       <S.NavCategoryContainer>
-        {CATEGORY_ICON.map(({ icon, text, className }, idx) => (
+        {CATEGORY_ICON.map(({ icon, text, className, router }, idx) => (
           <NavCategoriesIcon
             className={className}
             key={idx}
             icon={icon}
             text={text}
+            router={router}
+            accessToken={accessToken}
           />
         ))}
       </S.NavCategoryContainer>
     </S.Category>
   );
 }
-
-const CATEGORY_ICON = [
-  { icon: <IoMdInformationCircleOutline size="24px" />, text: '고사장 안내' },
-  { icon: <SlCalender size="24px" />, text: '캘린더' },
-  { icon: <ImMap2 size="24px" />, text: '오시는 길' },
-  {
-    icon: <CgList size="24px" />,
-    text: '나의 시험 일정',
-  },
-];
